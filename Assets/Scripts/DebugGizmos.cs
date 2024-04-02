b86ac020d71681dd9a1fc92bd789caf12bd66362
Nicolas Brueckner <nicolas.brueckner@thoughtfish.de>
2024-04-02 23:29:09 +0200

    added arrow sprites and some debug changes

--- Assets/Scripts/DebugGizmos.cs
+++ Assets/Scripts/DebugGizmos.cs
@@ -11,6 +11,7 @@ public class DebugGizmos : MonoBehaviour
 	public Color32 gridColor;
 
 	private Grid _currentGrid;
+	private int2 _gridOrigin;
 	private int2 _gridSize;
 	private float _cellHalfSize;
 
@@ -18,6 +19,7 @@ public class DebugGizmos : MonoBehaviour
 	public void SetFlowField(Grid grid)
 	{
 		_currentGrid = grid;
+		_gridOrigin = grid.gridOrigin;
 		_gridSize = grid.gridSize;
 		_cellHalfSize = grid.cellHalfSize;
 	}
@@ -27,20 +29,20 @@ public class DebugGizmos : MonoBehaviour
 		if ( showGrid )
 		{
 			if ( _currentGrid == null )
-				DrawGizmoGrid(gridController.gridSize, gridController.cellHalfSize);
+				DrawGizmoGrid(gridController.gridOrigin, gridController.gridSize, gridController.cellHalfSize);
 			else
-				DrawGizmoGrid(_gridSize, _cellHalfSize);
+				DrawGizmoGrid(_gridOrigin, _gridSize, _cellHalfSize);
 		}
 	}
 
-	private void DrawGizmoGrid(int2 gridSize, float cellHalfSize)
+	private void DrawGizmoGrid(int2 gridOrigin, int2 gridSize, float cellHalfSize)
 	{
 		Gizmos.color = gridColor;
 		for ( int x = 0; x < gridSize.x; x++ )
 		{
 			for ( int y = 0; y < gridSize.y; y++ )
 			{
-				Vector3 center = new float3(cellHalfSize * 2 * x + cellHalfSize, cellHalfSize * 2 * y + cellHalfSize, 0);
+				Vector3 center = new Vector3(gridOrigin.x + cellHalfSize * 2 * x + cellHalfSize, gridOrigin.y + cellHalfSize * 2 * y + cellHalfSize, 0);
 				Vector3 size = Vector3.one * cellHalfSize * 2;
 				Gizmos.DrawWireCube(center, size);
 			}
