f43d8013da6381eb556e014fe07ece0b20b8f905
Nicolas Brueckner <74188620+NicolasBrueckner@users.noreply.github.com>
2024-04-10 18:30:57 +0200

    some small singleton changes

--- Assets/Scripts/SectorData.cs
+++ Assets/Scripts/SectorData.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
+public enum SectorMap
+{
+	map_01,
+	map_02,
+	map_03,
+}
+
 public class SectorData
 {
+	private static readonly Lazy<SectorData> _instance = new Lazy<SectorData>( () => new SectorData() );
+	public static SectorData Instance => _instance.Value;
+
 	public Sector[] sectors;
-	public Sprite[] backgrounds;
-	public int2[] sizes;
 
 	private void InitializeSectors()
 	{
