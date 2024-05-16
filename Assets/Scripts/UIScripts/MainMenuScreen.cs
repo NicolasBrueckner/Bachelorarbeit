525f700dae07c5a216b2d3f17c3a1feceff323c8
Nicolas Brueckner <nicolas-brueckner@t-online.de>
2024-05-16 17:52:51 +0200

    all ui screens done and healthbar interaction
    health change event not implemented yet

--- Assets/Scripts/UIScripts/MainMenuScreen.cs
+++ Assets/Scripts/UIScripts/MainMenuScreen.cs
@@ -15,34 +15,25 @@ public class MainMenuScreen : MenuScreen
 	{
 		base.GetElements();
 
-		Debug.Log( "entered GetElements in MainMenuScreen" );
 		_startButton = _root_.Q<Button>( "Start" );
 		_quitButton = _root_.Q<Button>( "Quit" );
-
-		if ( _startButton != null )
-			Debug.Log( $"StartButton is not null, text on it: {_startButton.text}" );
 	}
 
 	protected override void BindElements()
 	{
 		base.BindElements();
 
-		if ( _startButton != null && _quitButton != null )
-		{
-			Debug.Log( "entered BindElements in MainMenuScreen" );
-			_startButton.clicked += OnStartButtonClicked;
-			_quitButton.clicked += OnQuitButtonClicked;
-		}
+		_startButton.clicked += OnStartButtonClicked;
+		_quitButton.clicked += OnQuitButtonClicked;
 	}
 
 	private void OnStartButtonClicked()
 	{
 		UIScreenManager.Instance.ShowScreen( UIScreenTypes.HUD );
-		Debug.LogWarning( "entered OnStartButtonClicked in MainMenuScreen" );
 	}
 
 	private void OnQuitButtonClicked()
 	{
-		Debug.LogWarning( "entered OnStartButtonClicked in MainMenuScreen" );
+		Application.Quit();
 	}
 }
