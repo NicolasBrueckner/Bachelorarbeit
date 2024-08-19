ac57eb4c65846d7da5f70b51b4625d2b7d80c5ca
Nicolas Brueckner <nicolas.brueckner@thoughtfish.de>
2024-08-19 20:09:57 +0200

    debug editor only

--- Assets/Scripts/RuntimeManager.cs
+++ Assets/Scripts/RuntimeManager.cs
@@ -14,8 +14,10 @@ public class RuntimeManager : MonoBehaviour
 	public EnemyPoolController enemyPoolController;
 	public FlowFieldController flowFieldController;
 	public WorldController worldController;
-	//public DebugController debugController;
 	public UpgradeController upgradeController;
+#if UNITY_EDITOR
+	public DebugController debugController;
+#endif
 
 	private GameObject _runtimeObjectCopy;
 
@@ -54,7 +56,9 @@ public class RuntimeManager : MonoBehaviour
 		enemyPoolController = FindObjectOfType<EnemyPoolController>();
 		flowFieldController = FindObjectOfType<FlowFieldController>();
 		worldController = FindObjectOfType<WorldController>();
-		//debugController = FindObjectOfType<DebugController>();
+#if UNITY_EDITOR
+		debugController = FindObjectOfType<DebugController>();
+#endif
 		upgradeController = FindObjectOfType<UpgradeController>();
 
 		InjectWeaponControllers();
